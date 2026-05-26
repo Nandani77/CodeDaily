c1dceb96f51c65fbb616cf3565e31673f4684481
Nandani77 <rajnandani24200204@gmail.com>
2026-05-26 12:58:08 +0530

    Added daily problems

--- js-6.js
+++ js-6.js
@@ -1,27 +1,51 @@
-function secondLargest(arr) {
+
+function secondLargest(arr) { 
 
     let largest = -1;
     let secondLargest = -1;
 
-    for (let i = 0; i < arr.length; i++) {
-
-        // New largest mila
+    for (let i = 0; i < arr.length; i++){
         if (arr[i] > largest) {
-
             secondLargest = largest;
             largest = arr[i];
-
-        }
-
-        // Second largest check
-        else if (arr[i] > secondLargest && arr[i] !== largest) {
-
+        } else if (arr[i] > secondLargest && arr[i] !== largest) {
             secondLargest = arr[i];
-
         }
+       
     }
-
     return secondLargest;
+
+  
 }
 
-console.log(secondLargest([12, 35, 1, 10, 34, 1]));
+console.log(secondLargest([12, 39, 1, 10, 36, 1]))
+
+
+
+// function secondLargest(arr) {
+
+//     let largest = -1;
+//     let secondLargest = -1;
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//         // New largest mila
+//         if (arr[i] > largest) {
+
+//             secondLargest = largest;
+//             largest = arr[i];
+
+//         }
+
+//         // Second largest check
+//         else if (arr[i] > secondLargest && arr[i] !== largest) {
+
+//             secondLargest = arr[i];
+
+//         }
+//     }
+
+//     return secondLargest;
+// }
+
+// console.log(secondLargest([12, 35, 1, 10, 34, 1]));
